d36f931dc3ad2285c98859dddd55d2d8c4b28f7f
BalliAsghar <ballihacker@outlook.com>
2021-12-06 15:03:07 +0000

    basic express app

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log(123);
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Index Route
+app.get("/", (req, res) => res.json({ message: "Hello" }));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
