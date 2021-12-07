ff3605f6eca4bdb8a104621d849c9fd856c85087
BalliAsghar <ballihacker@outlook.com>
2021-12-07 23:26:35 +0000

    comments on Categories Router

--- router/categoriesRouter.js
+++ router/categoriesRouter.js
@@ -5,11 +5,20 @@ const {
   handleWrongUrls,
 } = require("../errors/endpoint-errors");
 
-categoriesRouter // GET - /api/categories
-  .route("/")
-  .get(getCategories)
-  .all(handleWrongMethods);
+/**
+ * @Method GET
+ * @Route /api/categories
+ * @Description Get all categories
+ * @Function getCategories
+ */
+categoriesRouter.get("/", getCategories);
 
+/**
+ * @Method REST (POST, PUT, DELETE, PATCH)
+ * @Route /api/categories
+ * @Description Handle wrong methods
+ * @Function handleWrongMethods
+ */
 categoriesRouter.route("*").all(handleWrongUrls);
 
 module.exports = categoriesRouter;
