6f96bb743ad455a58066260520d8d74620aff21b
BalliAsghar <ballihacker@outlook.com>
2021-12-08 00:14:52 +0000

    typo

--- errors/index.js
+++ errors/index.js
@@ -10,7 +10,7 @@ module.exports = (err, req, res, next) => {
 
   // is stausCode is provided, use it, otherwise use the default status code of 500
   const statusCode = err.statusCode || 500;
-  // is message is provided, use it, otherwise use the default message of "Something went wrong"
+  // is message is provided, use it, otherwise use the default message of "Internal Server Error"
   const message = err.message || "Internal Server Error";
   // send the response with the status code and message
   res.status(statusCode).json({
