d38f548f134a3b09c9404698e1f8180bd2737cf0
hynesnd <hynesnd@gmail.com>
2021-12-08 12:59:35 +0000

    adds mock data for testing

---
+++ config/seed.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const connectdb = require("./db");
+const dotenv = require("dotenv");
+const User = require("./databaseConfig/user.schema");
+const Event = require("./databaseConfig/event.schema");
+const Comment = require("./databaseConfig/comment.schema");
+const Category = require("./databaseConfig/category.schema");
+const mockUsers = require("./mockData/MOCK_USERS.json");
+const mockEvents = require("./mockData/MOCK_EVENT.json");
+
+const seedData = async () => {
+  console.log("Begin seed Users");
+  console.log("clear and insert users");
+  await User.deleteMany({});
+  await User.insertMany(mockUsers);
+  console.log("clear and insert events");
+  await Event.deleteMany({});
+  await Event.insertMany(mockEvents);
+};
+
+mongoose
+  .connect(
+    "mongodb+srv://frostythesnowman2021:frostythesnowman2021@thesnowman.1ublf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("DB connection open");
+    return seedData();
+  })
+  .then(() => {
+    console.log("closing connection");
+    return mongoose.connection.close();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// seedData()
+//   .then(() => {
+//     console.log("closing connection");
+//     mongoose.connection.close();
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
