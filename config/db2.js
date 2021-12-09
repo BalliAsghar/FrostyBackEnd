40bfdad49d6bc8feb082cd467b294b2114f395bc
hynesnd <hynesnd@gmail.com>
2021-12-09 11:10:56 +0000

    deletes db2.js connection test file

--- config/db2.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-module.exports.connectDB = async () => {
-  await mongoose.connect(process.env.DB_URL);
-};
-
-module.exports.closeDB = async () => {
-  await mongoose.connection.close();
-};
